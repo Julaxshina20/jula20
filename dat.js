0c99ba6e65e393567bf0c98e04d496e1855b2846
Julaxshina20 <julakshanajula@gmail.com>
2025-06-12 11:37:48 +0530

    Add files via upload

---
+++ dat.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    var menu = document.getElementById("mobile-menu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+});
